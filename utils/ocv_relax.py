4340579df45227c3b0ef94c1414c0ac52d90705f
torvara <torvara@nmbu.no>
2016-08-16 15:10:39 +0200

    pushtest from sourcetree to github (#1)
    * pushtest from sourcetree to github
    
    * Made cellpy to a nicer title

---
+++ utils/ocv_relax.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+Adaption of OCV-relaxation data.
+"""
+
+__author__ = 'Tor Kristian Vara', 'Jan Petter Mæhlen'
+__email__ = 'tor.vara@nmbu.no', 'jepe@ife.no'
+
+
+def foo():
+    print "Hello world!"
+
+
+if __name__ == '__main__':
+    foo()   # This is a test
