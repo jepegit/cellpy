cd562a42e78e70a891c009b662e715c5baa591cf
AD\torkv <torkvara@gmail.com>
2016-08-17 15:13:55 +0200

    new print in to check if the branch is up to date with master

--- test/test_new_feature_branch.py
+++ test/test_new_feature_branch.py
@@ -8,3 +8,5 @@ __author__ = 'Tor Kristian Vara', 'Jan Petter Mæhlen'
 __email__ = 'tor.vara@nmbu.no', 'jepe@ife.no'
 
 print "This is a test done with cmd"
+print "Added a new test thing, but this time it'\s in the branch 'utils' and not test-new-feature-branch which now is" \
+      "deleted."
