de43b0e7380600d4149411b66cbf6aed14c05cfa
AD\torkv <torkvara@gmail.com>
2016-08-18 09:37:08 +0200

    making an example file where I can run some simple equations and python scripts

--- test/examples.py
+++ test/examples.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 
 """
-
+This file is made for doing examples in
 """
+
+import numpy as np
+import matplotlib.pyplot as plt
 
 __author__ = 'Tor Kristian Vara', 'Jan Petter Mæhlen'
 __email__ = 'tor.vara@nmbu.no', 'jepe@ife.no'
 
-print 'This is a test...'
+
+def rc_circuit():
+    pass
