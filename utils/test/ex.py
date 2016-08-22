e598f5afade86514a826feb7b7855587e79937f6
AD\torkv <torkvara@gmail.com>
2016-08-22 17:15:28 +0200

    still trying to plot some basic ocv relaxation curves from the cell sic006_45_cc_01

--- utils/test/ex.py
+++ utils/test/ex.py
@@ -13,15 +13,27 @@ __email__ = 'tor.vara@nmbu.no', 'jepe@ife.no'
 
 data = pd.read_csv(r'C:\Users\torkv\OneDrive - Norwegian University of Life '
                    r'Sciences\Documents\NMBU\master\ife\python\cellpy\utils'
-                   r'\data\20160805_sic006_45_cc_01_ocvrlx_down.csv', sep=';')
-df = pd.DataFrame(data)
+                   r'\data\20160805_sic006_45_cc_01_ocvrlx_down.csv',
+                   sep=';', header=None)
+df = pd.DataFrame(data[1:])
+time_data = np.transpose(np.array(df.loc[:, :: 2]))
+voltage_data = np.transpose(np.array(df.loc[:, 1::2]))
+
+xy_data = {'time': [], 'voltage': []}
+for time, volt in zip(time_data, voltage_data):
+    xy_data['time'].append(time)
+    xy_data['voltage'].append(volt)
+
+xy_df = pd.DataFrame(xy_data)
+print xy_df
+xy_df['voltage'].plot()
 
 # print df['time (s) cycle_no 1']
 # print df[['time (s) cycle_no 1','time (s) cycle_no 2']]
 # print np.array(df[['time (s) cycle_no 1','time (s) cycle_no 2']])
 
-df2 = pd.DataFrame(np.array(df[['time (s) cycle_no 1','time (s) cycle_no 2']]))
-print df2
+# df2 = pd.DataFrame(np.array(df[['time (s) cycle_no 1','time (s) cycle_no 2']]))
+# print df2
 # print df.head()
 # print df.tail()
 # print df.tail(2)
@@ -32,8 +44,11 @@ print df2
 #If I want to export, then write: df.to_csv('new_name.csv')
 # Renaming the coloumns
 
-df3 = pd.read_csv(r'C:\Users\torkv\OneDrive - Norwegian University of Life '
-                   r'Sciences\Documents\NMBU\master\ife\python\cellpy\utils'
-                   r'\data\20160805_sic006_45_cc_01_ocvrlx_down.csv', sep=';')
-df3.to_csv('new_csv.csv')
-df3 = pd.read_csv('')
+# df3 = pd.read_csv(r'C:\Users\torkv\OneDrive - Norwegian University of Life '
+#                    r'Sciences\Documents\NMBU\master\ife\python\cellpy\utils'
+#                    r'\data\20160805_sic006_45_cc_01_ocvrlx_down.csv', sep=';')
+# df3.to_csv('new_csv.csv')
+# df3 = pd.read_csv('new_csv.csv')
+# # print df.head()
+# df3.rename(columns={'time (s) cycle_no 1': 'time (s)'}, inplace=True)
+# print df3.head()
