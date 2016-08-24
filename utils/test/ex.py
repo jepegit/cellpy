30add3784b32da17a69155843ce6bceaa101ad58
AD\torkv <torkvara@gmail.com>
2016-08-24 09:16:32 +0200

    all plots work in ex and comments are made

--- utils/test/ex.py
+++ utils/test/ex.py
@@ -10,23 +10,27 @@ import matplotlib.pyplot as plt
 __author__ = 'Tor Kristian Vara', 'Jan Petter Mæhlen'
 __email__ = 'tor.vara@nmbu.no', 'jepe@ife.no'
 
-
+# had to use r'system path to file' to read the csv. Don't know why,
+# but that's how it is right now...
 data = pd.read_csv(r'C:\Users\torkv\OneDrive - Norwegian University of Life '
                    r'Sciences\Documents\NMBU\master\ife\python\cellpy\utils'
                    r'\data\20160805_sic006_45_cc_01_ocvrlx_down.csv',
-                   sep=';')
-data.to_csv('new_data')
-data = pd.read_csv('new_data', index_col=0)
-df = pd.DataFrame(data)
-
+                   sep=';', index_col=0)
+# need to separate time and voltage so that they can be plotted together as x-y
 time_data = [t for i in range(len(data.iloc[0, :])) for t in data.iloc[:, i]
-             if not i % 2]
-voltage_data = [v for k in range(len(data.iloc[0, :])) for v in data.iloc[:, k]
-                if k % 2]
+             if i == 0 or i % 2]
+voltage_data = [v for k in range(0, len(data.iloc[0, :]))
+                for v in data.iloc[:, k] if not k % 2]
+
+# Splitting the data so that they are in intervals of amount of cycles and
+# putting them into a dictionary for easier tracking. First cycle is the
+# first list. To call: ocv_dic['time/voltage'][0] etc.
 ocv_dic = {'time': [time_data[w: w + len(data)] for w in
                     xrange(0, len(time_data), len(data))],
            'voltage': [voltage_data[j: j + len(data)] for j in
                        xrange(0, len(voltage_data), len(data))]}
+
+# plotting all curves in same plot
 for _ in range(len(ocv_dic['time'])):
     plt.plot(ocv_dic['time'][_], ocv_dic['voltage'][_])
 
