90f12c6b477c3e2cf2a6acd2a77e45b97ecda4db
AD\torkv <torkvara@gmail.com>
2016-09-13 10:42:15 +0200

    out-commented a print

--- utils/cell_ocv.py
+++ utils/cell_ocv.py
@@ -357,10 +357,10 @@ if __name__ == '__main__':
         best_fit = ocv_relax_func(t_up, r_rc=p_u['r_rc'],
                                   c_rc=p_u['c_rc'], v_rlx=p_u['v_rlx'],
                                   ocv=p_u['ocv'])
-        print guess, popt_up[cycle_plot_up]
-        print '------------------------------------------------------------'
-        print guessed_fit, best_fit
-        print '============================================================'
+        # print guess, popt_up[cycle_plot_up]
+        # print '------------------------------------------------------------'
+        # print guessed_fit, best_fit
+        # print '============================================================'
         ocv_relax = np.array([guess['ocv'] + ocv_add for _ in range((len(
             t_up)))])
         subs[cycle_plot_up].plot(t_up, v_up, 'ob', t_up, guessed_fit, '--r',
