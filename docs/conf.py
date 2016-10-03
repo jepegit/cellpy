af031119443a25756df39df6f93d24c6c87590e6
jepe <jepe@ife.no>
2016-10-03 11:16:24 +0200

    edits doc/conf.py - corrects the name of the Sphinx extension for parsing google-type doc-strings

--- docs/conf.py
+++ docs/conf.py
@@ -55,7 +55,7 @@ import cellpy
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinxcontrib.napoleon']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.napoleon']
 
 # Napoleon settings
 napoleon_google_docstring = True
