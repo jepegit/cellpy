f570f7064d451c754c1dbbc76dfe7e557a599ba8
jepe <jepe@ife.no>
2016-10-29 13:33:05 +0200

    adds new tests (in develop, forgot to branch)

--- tests/test_readers.py
+++ tests/test_readers.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 import os
+import tempfile
+import shutil
 import pytest
 
 # -------- defining overall path-names etc ----------
@@ -10,6 +12,9 @@ test_res_file = "20160805_test001_45_cc_01.res"
 test_res_file_full = os.path.join(test_data_dir,test_res_file)
 test_data_dir_out = os.path.join(test_data_dir, "out")
 test_cellpy_file = "20160805_test001_45_cc.h5"
+test_cellpy_file_tmp = "tmpfile.h5"
+test_cellpy_file_full = os.path.join(test_data_dir,test_cellpy_file)
+test_cellpy_file_tmp_full = os.path.join(test_data_dir,test_cellpy_file_tmp)
 test_run_name = "20160805_test001_45_cc"
 
 # TODO: use only functions where logical (remove TestCase)
@@ -51,6 +56,81 @@ class TestDataReaders(TestCase):
         d = cellreader.cellpydata()
         d.set_cellpy_datadir(test_data_dir)
         assert test_data_dir == d.cellpy_datadir
+
+    def test_load_res(self):
+        from cellpy import cellreader
+        d = cellreader.cellpydata()
+        d.loadcell(test_res_file_full)
+        run_number = 0
+        data_point = 2283
+        step_time = 1500.05
+        sum_discharge_time = 362198.12
+        my_test = d.tests[run_number]
+        assert my_test.dfsummary.loc[1,"Data_Point"] == data_point
+        assert step_time == pytest.approx(my_test.dfdata.loc[4,"Step_Time"],0.1)
+        assert sum_discharge_time == pytest.approx(my_test.dfsummary.loc[:,"Discharge_Time"].sum(),0.1)
+        assert my_test.test_no == run_number
+
+        # d.make_summary(find_ir=True)
+        # d.create_step_table()
+        # d.save_test(test_cellpy_file_full)
+
+    def test_load_cellpyfile(self):
+        from cellpy import cellreader
+        d = cellreader.cellpydata()
+        d.load(test_cellpy_file_full)
+        run_number = 0
+        data_point = 2283
+        step_time = 1500.05
+        sum_test_time = 9301719.457
+        my_test = d.tests[run_number]
+        unique_cycles = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+        unique_cycles_read = my_test.step_table.loc[:, "cycle"].unique()
+        assert any(map(lambda v: v in unique_cycles_read, unique_cycles))
+        assert my_test.dfsummary.loc[1, "Data_Point"] == data_point
+        assert step_time == pytest.approx(my_test.dfdata.loc[4, "Step_Time"], 0.1)
+        assert sum_test_time == pytest.approx(my_test.dfsummary.loc[:, "Test_Time"].sum(), 0.1)
+        assert my_test.test_no == run_number
+
+    def test_save_cellpyfile_with_extension(self):
+        from cellpy import cellreader
+        d = cellreader.cellpydata()
+        d.loadcell(test_res_file_full)
+        d.make_summary(find_ir=True)
+        d.create_step_table()
+        tmp_file = next(tempfile._get_candidate_names())+".h5"
+        d.save_test(tmp_file)
+        assert os.path.isfile(tmp_file)
+        os.remove(tmp_file)
+        assert not os.path.isfile(tmp_file)
+
+    def test_save_cellpyfile_auto_extension(self):
+        from cellpy import cellreader
+        d = cellreader.cellpydata()
+        d.loadcell(test_res_file_full)
+        d.make_summary(find_ir=True)
+        d.create_step_table()
+        tmp_file = next(tempfile._get_candidate_names())
+        d.save_test(tmp_file)
+        assert os.path.isfile(tmp_file+".h5")
+        os.remove(tmp_file+".h5")
+        assert not os.path.isfile(tmp_file+".h5")
+
+    def test_save_cvs(self):
+        from cellpy import cellreader
+        d = cellreader.cellpydata()
+        d.loadcell(test_res_file_full)
+        d.make_summary(find_ir=True)
+        d.create_step_table()
+        temp_dir = tempfile.mkdtemp()
+        d.exportcsv(datadir=temp_dir)
+        shutil.rmtree(temp_dir)
+        # d.save_test(tmp_file)
+        # assert os.path.isfile(tmp_file)
+        # os.remove(tmp_file)
+        # assert not os.path.isfile(tmp_file)
+
+
 
 
     @pytest.mark.unfinished
