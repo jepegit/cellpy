e29713c38c655e856101e5fc67ba521ee5905df6
jepe <jepe@ife.no>
2017-04-04 10:41:13 +0200

    cleans up ica.py (adds some docstrings, makes it py3 comp.)

---
+++ cellpy/utils/batch.py
@@ -0,0 +1,9 @@
+"""Routines for batch processing of cells"""
+import os
+import warnings
+import logging
+
+logging.captureWarnings(True)
+
+if __name__ == '__main__':
+    warnings.warn("to be implemented",)
