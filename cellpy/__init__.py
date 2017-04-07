be76d1b43d9664e80bf0930f7ae59ea4c9b829d2
jepegit <jepe@ife.no>
2017-04-07 20:30:43 +0200

    Update __init__.py

--- cellpy/__init__.py
+++ cellpy/__init__.py
@@ -18,4 +18,4 @@ __all__ = ["cellreader", "dbreader", "prmreader", "filefinder"]
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
-# I wonder if adding prmreader.read() here makes the prms available as cellpy.parameters ?
+# I wonder if adding prmreader.read() here makes the prms available as cellpy.parametres?
