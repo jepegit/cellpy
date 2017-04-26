674c649e4311bd6cc461ae2ce87f0c60dde4bf9c
jepe <jepe@ife.no>
2017-04-26 14:42:42 +0200

    creates empty test to learn more about how Travis works

---
+++ tests/test_nothing.py
@@ -0,0 +1,31 @@
+import os
+import tempfile
+import pytest
+
+# -------- defining overall path-names etc ----------
+current_file_path = os.path.dirname(os.path.realpath(__file__))
+relative_test_data_dir = "../cellpy/data_ex"
+test_data_dir = os.path.abspath(os.path.join(current_file_path, relative_test_data_dir))
+test_res_file = "20160805_test001_45_cc_01.res"
+test_res_file_full = os.path.join(test_data_dir,test_res_file)
+test_data_dir_out = os.path.join(test_data_dir, "out")
+test_cellpy_file = "20160805_test001_45_cc.h5"
+test_cellpy_file_tmp = "tmpfile.h5"
+test_cellpy_file_full = os.path.join(test_data_dir,test_cellpy_file)
+test_cellpy_file_tmp_full = os.path.join(test_data_dir,test_cellpy_file_tmp)
+test_run_name = "20160805_test001_45_cc"
+
+import logging
+from cellpy import log
+from cellpy.utils import ica
+
+log.setup_logging(default_level=logging.DEBUG)
+
+
+def test_empty():
+    r = None
+    assert r is None
+
+
+
+
