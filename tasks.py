aad0b7bc658faf2b4ae52ecf822c755a4494079d
Jan Petter Maehlen <jepe@ife.no>
2019-08-18 13:08:06 +0200

    manual

--- tasks.py
+++ tasks.py
@@ -42,6 +42,7 @@ def get_platform():
 def m(c, comment="automatic commit"):
     cos = get_platform()
     print(f"Running on platform: {cos}")
+
     print(" status ".center(80, "-"))
     c.run("git status")
     print(" staging ".center(80, "-"))
