98a12b2dbcf8f0c91238253b02f41ebce420a0fc
amundmr <amund.raniseth@gmail.com>
2021-07-01 16:00:51 +0200

    getting to know cellpy functions

--- cellpy/utils/easyplot.py
+++ cellpy/utils/easyplot.py
@@ -4,11 +4,20 @@
 import os
 from pathlib import Path
 import logging
-
 import cellpy
+import matplotlib.pyplot as plt
+
+
+#### WORK IN PROGRESS ####
 
 def plot(files):
     cellpyobjects = []
     for file in files:
         cellpyobjects.append(cellpy.get(filename = file))
     print("plot func is called!")
+    print(cellpyobjects)
+    print(cellpyobjects[0].get_cycle_numbers())
+    curves1 = cellpyobjects[0].get_cap(method="forth-and-forth")
+    print(curves1)
+    print(type(curves1))
+    curves1.plot()
