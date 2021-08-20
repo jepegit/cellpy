a6ac96575e28ab421b0017972629235f230d133b
Amund Midtgard Raniseth <54708595+amundmr@users.noreply.github.com>
2021-08-20 14:03:05 +0200

    Merging development of easyplot into master branch (#171)
    * 1st commit in separate dev branch, keeping master clean
    
    * Added possibility for ratecap-capacitydetermination
    
    * Added custom file extension possibility
    
    * Pythonized all my oldfashioned if statements
    
    * Added 20 color colorpalette
    
    * removed white from the extended color palette
    
    * Added normalization feature to GCPL curves, bugged when plotting both charge and discharge curves.
    
    * Tidy up so noone sees my previous print statements
    
    * Normalizing cap wasn't impl in separate plot style, fixed now.
    
    * Added function for linregress on degradation. Functional both for percentage and normal capacity
    
    * Updated SQL driver to a more reliabla one
    
    * mall change
    
    * added nickname feature to gc plots
    
    * added custom filename feature, removed charge from RC-cap analysis
    
    * made only_chg/dischg work on cyclelife
    
    * WARNING:RC CAP DET IS NOW DEPENDENT ON 5 CYCLES PER C-RATE, this should almost be issued as a bug..
    
    * Exponential fit to determine capacity from ratecapability has been removed. closes #167
    
    * closes #170, you can now use easyplot.help() directly
    
    * closes #168, implemented separate cyclelife plotting by param cyclelife_separate_data
    
    * added some docstrings:)
    
    * sorted imports:)
    
    * fixed class docstr
    
    * touching up comments and docstrings

--- cellpy/utils/run_easyplot.py
+++ cellpy/utils/run_easyplot.py
@@ -5,73 +5,86 @@ if __name__ == "__main__":
     import cellpy
     import easyplot
 
-    files = [
-        # "./data/raw/20160805_test001_45_cc_01.res",
-        # "./data/raw/20160805_test001_45_cc_01_copy.res",
-        # "./data/20210430_seam10_01_01_cc_01_Channel_48_Wb_1.xlsx.csv",
-        # "./data/20210430_seam10_01_02_cc_01_Channel_49_Wb_1.xlsx.csv"
-        # "20210624_seam12_07_01_cc_02",
-        # "20210624_seam12_07_02_cc_02",
-        # "20210624_seam12_07_03_cc_02",
-        # "20210628_seam12_04_01_cc_01",
-        # "20210628_seam12_04_02_cc_02",
-        # "20210628_seam12_04_03_cc_01",
-        # "20210628_seam12_05_01_cc_01",
-        # "20210628_seam12_05_02_cc_01",
-        # "20210628_seam12_05_03_cc_01",
-        # "20210628_seam12_06_01_cc_01",
-        # "20210628_seam12_06_02_cc_02",
-        # "20210628_seam12_06_03_cc_01",
-        "20210628_seam12_07_01_cc_01",
-        "20210628_seam12_07_02_cc_01",
-        "20210628_seam12_07_03_cc_01",
-        "20210630_seam13_01_01_cc_01",
-        # "20210630_seam13_01_02_cc_01",
-        # "20210630_seam13_01_03_cc",
-        # "20210630_seam13_02_01_cc_01",
-        # "20210630_seam13_02_02_cc_01",
-        # "20210630_seam13_02_03_cc_01",
-        # "20210630_seam13_03_01_cc_01",
-        # "20210630_seam13_03_02_cc_01",
-        # "20210630_seam13_03_03_cc_01",
-        # "20210630_seam13_04_01_cc_01",
-        # "20210630_seam13_04_02_cc_01",
-        # "20210630_seam13_04_03_cc_01",
-    ]
+    #easyplot.help()
 
-    nicknames = None  # ["Seam10_01_01", "Seam10_01_02"]
+    files = [   
+    #'20210624_seam12_07_01_cc_02',
+    # === SEAM 17 1ST GC ===
+    #'20210812_seam17_07_gc_01_cc_01',
+    #'20210812_seam17_07_gc_02_cc_01',
+    #'20210812_seam17_07_gc_03_cc_01',
+    #'20210812_seam17_08_gc_01_cc_01',
+    #'20210812_seam17_08_gc_02_cc_01',
+    #'20210812_seam17_08_gc_03_cc_01',
+        # === SEAM 12 RATE CAPABILITY ===
+    #'20210712_seam12_04_rc_01_cc_01',
+    #'20210712_seam12_04_rc_02_cc_01',
+    #'20210712_seam12_04_rc_03_cc_01',
+    #'20210712_seam12_05_rc_01_cc_01',
+    #'20210712_seam12_05_rc_02_cc_01',
+    #'20210712_seam12_05_rc_03_cc_01',
+    #'20210712_seam12_06_rc_01_cc_01',
+    #'20210712_seam12_06_rc_02_cc_01',
+    #'20210712_seam12_06_rc_03_cc_01',
+    #'20210712_seam12_07_rc_01_cc_01',
+    #'20210712_seam12_07_rc_02_cc_01',
+    #'20210712_seam12_07_rc_03_cc_01',
+    # === SEAM 12 1ST GC ===
+    '20210628_seam12_04_01_cc_01',
+    #'20210628_seam12_04_02_cc_02',
+    #'20210628_seam12_04_03_cc_01',
+    '20210628_seam12_05_01_cc_01',
+    #'20210628_seam12_05_02_cc_01',
+    #'20210628_seam12_05_03_cc_01',
+    #'20210628_seam12_06_01_cc_01',
+    #'20210628_seam12_06_02_cc_01',
+    '20210628_seam12_06_03_cc_01',
+    #'20210628_seam12_07_01_cc_01',
+    #'20210628_seam12_07_02_cc_01',
+    #'20210628_seam12_07_03_cc_01',
+    #'./dev_data/DPV_test_1_SEAM12_04_02-testcell.mpr',
+    #'./20160805_test001_47_cc_01.res',
+            ]
 
-    ezplt = easyplot.EasyPlot(
-        files,
-        nicknames,
-        cyclelife_plot=True,
-        cyclelife_percentage=True,
-        cyclelife_coulombic_efficiency=True,
-        cyclelife_coulombic_efficiency_ylabel="Coulombic efficiency [%]",
-        cyclelife_xlabel="Cycles",
-        cyclelife_ylabel=r"Capacity $\left[\frac{mAh}{g}\right]$",
-        cyclelife_ylabel_percent="Capacity retention [%]",
-        cyclelife_legend_outside=True,  # if True, the legend is placed outside the plot
-        galvanostatic_plot=True,
-        galvanostatic_potlim=(0, 1),  # min and max limit on potential-axis
-        galvanostatic_caplim=None,
-        galvanostatic_xlabel=r"Capacity $\left[\frac{mAh}{g}\right]$",
-        galvanostatic_ylabel="Cell potential [V]",
-        dqdv_plot=True,
-        dqdv_potlim=None,  # min and max limit on potential-axis
-        dqdv_dqlim=None,
-        dqdv_xlabel="Cell potential [V]",
-        dqdv_ylabel=r"dQ/dV $\left[\frac{mAh}{gV}\right]$",
-        specific_cycles=None,  # []
-        exclude_cycles=[1, 5, 10, 11, 12],  # []
-        all_in_one=True,
-        only_dischg=True,
-        only_chg=False,
-        outpath="./ezplots/deleteme/",
-        figsize=(6, 4),  # 6 inches wide, 4 inches tall
-        figres=100,  # Dots per inch
-        figtitle=None,  # None = original filepath
+    nicknames = None #["04 Pure Si", "05 Low Carbon", "06 Medium Carbon", "07 High Carbon"]
+
+    ezplt = easyplot.EasyPlot(files, nicknames,
+        cyclelife_plot = True,
+        cyclelife_separate_data = True,
+        cyclelife_percentage = False,
+        cyclelife_coulombic_efficiency = True,
+        cyclelife_coulombic_efficiency_ylabel = "Coulombic efficiency [%]",
+        cyclelife_charge_c_rate = False,
+        cyclelife_discharge_c_rate = False,
+        cyclelife_ir = False, # NOT FINISHED
+        cyclelife_xlabel = "Cycles",
+        cyclelife_ylabel = r"Capacity $\left[\mu Ah\right]$",
+        cyclelife_ylabel_percent = "Capacity retention [%]",
+        cyclelife_legend_outside = False,         # if True, the legend is placed outside the plot
+        cyclelife_degradation_slope = False,
+        capacity_determination_from_ratecap = False,
+        galvanostatic_plot = False,
+        galvanostatic_potlim = (0,1),     #min and max limit on potential-axis
+        galvanostatic_caplim = None,
+        galvanostatic_xlabel = r"Capacity $\left[\mu Ah \right]$",
+        galvanostatic_ylabel = "Cell potential [V]",
+        galvanostatic_normalize_capacity = False,
+        dqdv_plot = False,
+        dqdv_potlim = (0,1),     #min and max limit on potential-axis
+        dqdv_dqlim = (-1000,1000),
+        dqdv_xlabel = "Cell potential [V]",
+        dqdv_ylabel = r"dQ/dV $\left[\frac{\mu Ah}{V}\right]$",
+        specific_cycles = None,#[]
+        exclude_cycles = None,#[]
+        all_in_one = False,
+        only_dischg = False,
+        only_chg = False,
+        outpath = "./ezplots/dev_crate/",
+        outtype = ".svg",
+        figsize = (6,4), # 6 inches wide, 4 inches tall
+        figres = 100, # Dots per inch
+        figtitle = None, # None = original filepath
     )
-    ezplt.set_arbin_sql_credentials("localhost", "sa", "Amund1234", "SQL Server")
+    ezplt.set_arbin_sql_credentials("localhost", "dnuma", "amund1234")
     # ezplt.help()
     ezplt.plot()
