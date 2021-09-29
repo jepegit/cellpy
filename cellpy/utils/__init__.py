b92aa33e77a402f7bc9855307a5abc93a55d5ffa
streamengineer <90567734+streamengineer@users.noreply.github.com>
2021-09-29 21:46:07 +0200

    Delete cellpy directory

--- cellpy/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""
-
-"""
-
-__author__ = "Tor Kristian Vara", "Jan Petter Maehlen", "Asbjoern Ulvestad"
-__email__ = "tor.vara@nmbu.no", "jepe@ife.no", "Asbjorn.Ulvestad@ife.no"
