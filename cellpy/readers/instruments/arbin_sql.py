67d80c89fbc1b49a541319043d84b9888b1e1a17
jepegit <jepe@ife.no>
2022-06-02 10:16:59 +0200

    Comment about arbin_sql.py

--- cellpy/readers/instruments/arbin_sql.py
+++ cellpy/readers/instruments/arbin_sql.py
@@ -301,6 +301,7 @@ class ArbinSQLLoader(Loader):
         if rename_headers:
             columns = {}
             for key in self.arbin_headers_normal:
+                # TODO: check what actually happens here (old_header = None -> columns[None] ?)
                 old_header = normal_headers_renaming_dict.get(key,
                                                               None)  # changed from normal_headers_renaming_dict[key] to get around the KeyError
                 new_header = self.cellpy_headers_normal[key]
