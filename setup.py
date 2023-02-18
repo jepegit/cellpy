6a88b5a50eb3c6cfc2fb6db17448748d87d28878
jepegit <jepe@ife.no>
2023-02-18 21:04:38 +0100

    min py3.9

--- setup.py
+++ setup.py
@@ -101,7 +101,6 @@ setup(
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
