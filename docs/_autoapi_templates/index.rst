a233282455163e969d64f02fcc2a4f8c24a566f3
jepegit <jepe@ife.no>
2024-02-19 08:56:58 +0100

    modify autoapi settings

--- docs/_autoapi_templates/index.rst
+++ docs/_autoapi_templates/index.rst
@@ -1,7 +1,9 @@
 API Reference
 =============
 
-This page contains auto-generated API reference documentation [#f1]_.
+.. note::
+
+    This page contains auto-generated API reference documentation [#f1]_.
 
 .. toctree::
    :titlesonly:
--- docs/conf.py
+++ docs/conf.py
@@ -337,3 +337,9 @@ texinfo_documents = [
 nbsphinx_kernel_name = "python3"
 autoapi_dirs = ["../cellpy"]
 autoapi_template_dir = "_autoapi_templates"
+autoapi_options = [
+    "members",
+    "undoc-members",
+    "show-inheritance",
+    "show-module-summary",
+]
