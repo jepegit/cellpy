23fe28a5796e6f45725880ff421efc934a9900f4
jepegit <jepe@ife.no>
2024-02-20 10:18:09 +0100

    fix setup and manifest (rst -> md)

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from setuptools import find_packages, setup
 with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open("HISTORY.rst") as history_file:
+with open("HISTORY.md") as history_file:
     history = history_file.read()
 
 included_packages = find_packages(
