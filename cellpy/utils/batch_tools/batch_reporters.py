b821f3bab782049909408044b6c17203c15c2e24
jepegit <jepe@ife.no>
2025-04-03 21:34:26 +0200

    update readthedocs.yaml (miniconda-latest)

--- cellpy/utils/batch_tools/batch_reporters.py
+++ cellpy/utils/batch_tools/batch_reporters.py
@@ -5,7 +5,19 @@ class HTMLReporter(BaseReporter):
     def __init__(self):
         super().__init__()
 
+    def run_engine(self, engine):
+        pass
+
+    def run_dumper(self, dumper):
+        pass
+
 
 class PPTReporter(BaseReporter):
     def __init__(self):
         super().__init__()
+
+    def run_engine(self, engine):
+        pass
+
+    def run_dumper(self, dumper):
+        pass
